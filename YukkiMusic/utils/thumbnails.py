b742372a847c5c320dc4707cbfff5c400c872698
Dhimas Azman <85933622+dhimasazman@users.noreply.github.com>
2022-05-09 00:09:29 +0700

    a

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -94,7 +94,7 @@ async def gen_thumb(videoid):
         )
         draw.text(
             (600, 150),
-            "NOW PLAYING",
+            "SEDANG MEMUTAR",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
@@ -124,13 +124,13 @@ async def gen_thumb(videoid):
 
         draw.text(
             (600, 450),
-            f"Views : {views[:23]}",
+            f"Jumlah Penonton : {views[:23]}",
             (255, 255, 255),
             font=arial,
         )
         draw.text(
             (600, 500),
-            f"Duration : {duration[:23]} Mins",
+            f"Durasi : {duration[:23]} Mins",
             (255, 255, 255),
             font=arial,
         )
